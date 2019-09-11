df7126758ef56841b03e374692cde6b6f2f5629c
Evan Qin <evan@EvandeMacBook-Pro.local>
2019-09-11 09:38:56 +0800

    遍历

---
+++ 遍历.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 
+#coding=utf-8
+import os
+import os.path
+rootdir="/Users/evan/Downloads"
+_list=os.listdir(rootdir)
+for i in range(0,len(_list)):
+    prth=os.path.join(rootdir,list[i])
+    if os.path.isfile(path):
+        print()
